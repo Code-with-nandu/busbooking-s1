d2b29d1a3836b6568cf8d0990bce9c4fe3ee21bf
Krishnendu <krishnendudalui@gmail.com>
2024-10-07 22:44:07 +0530

    final submit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/1_CI/busbooking';
+// $config['base_url'] = 'http://localhost/1_CI/busbooking';
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $config['base_url'] = 'http://localhost/1_CI/busbooking';
+} else {
+    $config['base_url'] = 'https://krishnendudalui.in.net/busbooking-s1/';
+}
+
 /*
 |--------------------------------------------------------------------------
 | Index File
